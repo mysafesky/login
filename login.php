6ee2fc2832b451a7f6940f503b4004e7455a8bfc
root <root@ip-172-31-11-49.us-west-1.compute.internal>
2015-09-20 03:24:42 +0000

    Add real login jump

--- login.php
+++ login.php
@@ -1,17 +1,18 @@
 <?php
 	session_start(); // Starting Session
 	$error=''; // Variable To Store Error Message
-	if (isset($_POST['submit'])) {
+        if (isset($_POST['submit'])) {
 		if (empty($_POST['username']) || empty($_POST['password'])) {
 			$error = "Invalid Account and Password combination";
-		}
+                //  	$error = var_dump(_POST);
+          	}
 		else
 		{
 			// Define $username and $password
 			$username=$_POST['username'];
 			$password=$_POST['password'];
 			// Establishing Connection with Server by passing server_name, user_id and password as a parameter
-			$connection = mysqli_connect("localhost:8889", "root", "root","company");
+			$connection = mysqli_connect("localhost", "root", "2500181","dev");
 			// To protect MySQL injection for Security purpose
 			$username = stripslashes($username);
 			$password = stripslashes($password);
@@ -22,10 +23,12 @@
 			$login_result = mysqli_query($connection,"select * from login where password='$password' AND username='$username'");
 			if ($login_result) {
 				$_SESSION['login_user']=$username; // Initializing Session
-				header("location: profile.php"); // Redirecting To Other Page
+				header("location: ../aqi_apiUI/"); // Redirecting To Other Page
 			} else {
 				$error = "Invalid Account and Password combination";
+                                //$error = var_dump(_POST);
 			}
+
 			mysqli_close($connection); // Closing Connection
 		}
 	}
